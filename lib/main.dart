7f4ccf12a94d9ae2bd0266c4bff395d6fccd0803
Nawodya Jayalath <nawodyahckr@gmail.com>
2020-06-12 22:38:06 +0530

    background color to teal

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+  @override 
+  Widget build(BuildContext buildcontext){
+    return MaterialApp(
+      home:Scaffold(
+        backgroundColor: Colors.teal,
+      ),
+    );
+  }
+}
